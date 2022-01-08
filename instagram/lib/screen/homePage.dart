356361aec6740ade097adfcc9c05a0a7c635cba7
shweta kc <shweta.kc016@gmail.com>
2022-01-08 10:28:13 +0545

    Add app Bar on home page

--- instagram/lib/screen/homePage.dart
+++ instagram/lib/screen/homePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,9 +11,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomePage'),
-      ),
+      appBar: getAppBar(),
+      body: getBody(),
     );
+  }
+
+  Widget getAppBar() {
+    return PreferredSize(
+      child: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.fromLTRB(8, 20, 8, 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SvgPicture.asset(
+              'assets/images/logo.svg',
+              width: 90,
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(FontAwesome5Brands.facebook_messenger))
+          ],
+        ),
+      )),
+      preferredSize: Size.fromHeight(70),
+    );
+  }
+
+  Widget getBody() {
+    return Container();
   }
 }
