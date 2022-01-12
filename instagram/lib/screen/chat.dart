206a81dbc332c3b7e16f08b28fe14b796cb58ebd
shweta kc <shweta.kc016@gmail.com>
2022-01-12 20:35:40 +0545

    Add app bar

--- instagram/lib/screen/chat.dart
+++ instagram/lib/screen/chat.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:instagram/color.dart';
 
 class Chat extends StatefulWidget {
-
   @override
   _ChatState createState() => _ChatState();
 }
@@ -10,7 +11,51 @@ class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('chat'),),
+      appBar: getAppBar(),
+    );
+  }
+
+  Widget getAppBar() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(55),
+      child: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: textBlack,
+                  )),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                'Chats',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              IconButton(
+                splashRadius: 15,
+                onPressed: (){}, icon: Icon(Feather.video),
+                ),
+                SizedBox(width: 5,),
+                IconButton(
+                splashRadius: 15,
+                onPressed: (){}, icon: Icon(Feather.edit),
+                ),
+            ],
+          )
+        ],
+      )),
     );
   }
 }
