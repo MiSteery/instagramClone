ba4157e5ce5c591a8cc36a57faa97bd1ba542afc
shweta kc <shweta.kc016@gmail.com>
2022-01-13 21:40:02 +0545

    add appbar and back arrow

--- instagram/lib/screen/chatDetail.dart
+++ instagram/lib/screen/chatDetail.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:instagram/color.dart';
 
 class ChatDetail extends StatefulWidget {
 
@@ -9,10 +11,28 @@ class ChatDetail extends StatefulWidget {
 class _ChatDetailState extends State<ChatDetail> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-     
+      appBar: getAppBar(),
     );
   }
 
-
+  Widget getAppBar(){
+    return PreferredSize(
+       preferredSize: Size.fromHeight(55),
+      child: SafeArea(
+        child:Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_back, color: textBlack,),
+                onPressed: () {
+                    Navigator.pop(context);
+                  },
+            
+            )
+          ],
+        )
+        ),
+      );
+  }
 }
