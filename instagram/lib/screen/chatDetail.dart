da806906c8c8fd5cdeae19afe9a26b1b7d708c3f
shweta kc <shweta.kc016@gmail.com>
2022-01-13 21:48:29 +0545

    Add user profile and name

--- instagram/lib/screen/chatDetail.dart
+++ instagram/lib/screen/chatDetail.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:instagram/color.dart';
+import 'package:instagram/model/chat.dart';
 
 class ChatDetail extends StatefulWidget {
-
   @override
   _ChatDetailState createState() => _ChatDetailState();
 }
@@ -17,22 +16,40 @@ class _ChatDetailState extends State<ChatDetail> {
     );
   }
 
-  Widget getAppBar(){
+  Widget getAppBar() {
     return PreferredSize(
-       preferredSize: Size.fromHeight(55),
+      preferredSize: Size.fromHeight(55),
       child: SafeArea(
-        child:Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.arrow_back, color: textBlack,),
-                onPressed: () {
-                    Navigator.pop(context);
-                  },
-            
-            )
-          ],
-        )
-        ),
-      );
+          child: Row(
+        children: [
+          IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: textBlack,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          SizedBox(width: 10),
+          Container(
+            height: 33,
+            width: 33,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  image: NetworkImage(chats[0]['profile']), fit: BoxFit.cover),
+            ),
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Text(
+            chats[0]['username'],
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          )
+        ],
+      )),
+    );
   }
 }
