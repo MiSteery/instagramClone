cebb965ed04b7f7e27e9145b31df8755e0e09899
shweta kc <shweta.kc016@gmail.com>
2022-01-13 21:35:53 +0545

    initial commit

---
+++ instagram/lib/screen/chatDetail.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ChatDetail extends StatefulWidget {
+
+  @override
+  _ChatDetailState createState() => _ChatDetailState();
+}
+
+class _ChatDetailState extends State<ChatDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+    );
+  }
+
+
+}
